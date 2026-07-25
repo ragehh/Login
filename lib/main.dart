3dfab8fee91be33af2f4bd8cada68d577cade496
ragehh <mrageh68@gmail.com>
2026-07-25 20:44:18 +0300

    Integrate DevicePreview for enhanced UI testing and add device_preview dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:login/core/theme/app_theme.dart';
 import 'package:login/features/auth/presentation/views/login_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +20,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Login Page',
       theme: AppTheme.light,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: const LoginView(),
     );
   }
